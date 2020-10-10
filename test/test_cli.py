8e9f04c1cbd1551eb5bed248396c4f42626c165a
Per Høgfeldt <hogfeldt38@gmail.com>
2020-10-10 14:16:26 +0200

    Added test for cli interface

---
+++ test/test_cli.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest import TestCase
+from click.testing import CliRunner
+
+import ctDNAtool.cli as cli
+
+class Test_cli(TestCase):
+    def test_help(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.cli)
+        self.assertTrue(result.exit_code == 0)
+
