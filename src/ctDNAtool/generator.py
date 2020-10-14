ec65240485678f152183a8cb19cb43a525421570
Per Høgfeldt <hogfeldt38@gmail.com>
2020-10-14 13:57:24 +0200

    Generators refactored to use Data abstraction

--- src/ctDNAtool/generator.py
+++ src/ctDNAtool/generator.py
@@ -4,7 +4,7 @@ import attr
 from scipy.sparse import csr_matrix, lil_matrix
 
 from .bam import BAM
-from .utils import write_matrix_and_index_file
+from .data import Data
 
 
 @attr.s
@@ -12,7 +12,7 @@ class BED:
     chrom = attr.ib()
     start = attr.ib()
     end = attr.ib()
-    tss_id = attr.ib()
+    region_id = attr.ib()
     score = attr.ib()
     strand = attr.ib()
 
@@ -84,10 +84,10 @@ def generate_end_length_tensor(bam_file, bed_file, output_file, max_length):
     bed_list = load_bed_file(bed_file)
     region_size = bed_list[0].end - bed_list[0].start
     bam = BAM(bam_file)
-    index_lst = list()
+    id_lst = list()
     matrix_lst = list()
-    for region_index, region in enumerate(bed_list):
-        tss = int(region.tss_id.split("_")[-1])
+    for region in bed_list:
+        tss = int(region.region_id.split("_")[-1])
         region_matrix = lil_matrix((max_length, region_size), dtype=np.uint16)
         for reading in bam.pair_generator(region.chrom, region.start, region.end):
             start = int(reading[1])
@@ -101,9 +101,9 @@ def generate_end_length_tensor(bam_file, bed_file, output_file, max_length):
                 if bp_index >= 0 and bp_index < region_size:
                     region_matrix[length, bp_index] += 1
         matrix_lst.append(csr_matrix(region_matrix))
-        index_lst.append((region_index, region.tss_id))
+        id_lst.append(region.region_id)
     tensor = np.array(matrix_lst)
-    write_matrix_and_index_file(output_file, tensor, index_lst)
+    Data.write(Data(tensor, id_lst), output_file)
 
 
 def generate_length_matrix(bam_file, bed_file, output_file, max_length=500):
@@ -127,7 +127,7 @@ def generate_length_matrix(bam_file, bed_file, output_file, max_length=500):
     bed_list = load_bed_file(bed_file)
     matrix = np.zeros((len(bed_list), max_length), dtype=np.uint16)
     bam = BAM(bam_file)
-    index_lst = list()
+    id_lst = list()
     for i, region in enumerate(bed_list):
         for reading in bam.pair_generator(region.chrom, region.start, region.end):
             start = int(reading[1])
@@ -135,8 +135,8 @@ def generate_length_matrix(bam_file, bed_file, output_file, max_length=500):
             length = abs(end - start)
             if length < max_length:
                 matrix[i, length] += 1
-        index_lst.append((i, region.tss_id))
-    write_matrix_and_index_file(output_file, matrix, index_lst)
+        id_lst.append(region.region_id)
+    Data.write(Data(matrix, id_lst), output_file)
 
 
 def generate_read_ends_matrix(bam_file, bed_file, output_file):
@@ -159,9 +159,9 @@ def generate_read_ends_matrix(bam_file, bed_file, output_file):
     region_size = bed_list[0].end - bed_list[0].start
     coverage_matrix = np.zeros((len(bed_list), region_size), dtype=np.uint16)
     bam = BAM(bam_file)
-    index_lst = list()
+    id_lst = list()
     for i, region in enumerate(bed_list):
-        tss = int(region.tss_id.split("_")[-1])
+        tss = int(region.region_id.split("_")[-1])
         for reading in bam.pair_generator(region.chrom, region.start, region.end):
             read_start = int(reading[1])
             read_end = int(reading[2])
@@ -170,8 +170,8 @@ def generate_read_ends_matrix(bam_file, bed_file, output_file):
             )
             k = tss - region.start
             add_read_ends(coverage_matrix, rel_start, rel_end, i, k)
-        index_lst.append((i, region.tss_id))
-    write_matrix_and_index_file(output_file, coverage_matrix, index_lst)
+        id_lst.append(region.region_id)
+    Data.write(Data(coverage_matrix, id_lst), output_file)
 
 
 def generate_coverage_matrix(bam_file, bed_file, output_file):
@@ -194,9 +194,9 @@ def generate_coverage_matrix(bam_file, bed_file, output_file):
     region_size = bed_list[0].end - bed_list[0].start
     coverage_matrix = np.zeros((len(bed_list), region_size), dtype=np.uint16)
     bam = BAM(bam_file)
-    index_lst = list()
+    id_lst = list()
     for i, region in enumerate(bed_list):
-        tss = int(region.tss_id.split("_")[-1])
+        tss = int(region.region_id.split("_")[-1])
         for reading in bam.pair_generator(region.chrom, region.start, region.end):
             read_start = int(reading[1])
             read_end = int(reading[2])
@@ -205,5 +205,5 @@ def generate_coverage_matrix(bam_file, bed_file, output_file):
             )
             k = tss - region.start
             add_fragment(coverage_matrix, rel_start, rel_end, i, k)
-        index_lst.append((i, region.tss_id))
-    write_matrix_and_index_file(output_file, coverage_matrix, index_lst)
+        id_lst.append(region.region_id)
+    Data.write(Data(coverage_matrix, id_lst), output_file)
