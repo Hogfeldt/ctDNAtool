52415652dbc92ada27b29bee91d445551f8d1423
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2020-11-25 15:34:23 +0100

    Added option for binning genome in chromosomes

--- src/ctDNAtool/generators/__init__.py
+++ src/ctDNAtool/generators/__init__.py
@@ -2,8 +2,4 @@ from .length_matrix import length_matrix
 from .length_end_seqs import length_end_seqs
 from .mate_length_end_seqs import mate_length_end_seqs
 
-__all__ = [
-    "length_matrix",
-    "length_end_seqs",
-    "mate_length_end_seqs",
-]
+__all__ = ["length_matrix", "length_end_seqs", "mate_length_end_seqs"]
--- src/ctDNAtool/preprocessors/bin_genome.py
+++ src/ctDNAtool/preprocessors/bin_genome.py
@@ -1,9 +1,9 @@
-from enum import Enum, auto
 import py2bit
 from natsort import natsorted
+from enum import Enum, auto
 
 from .utils import is_autosome, is_autosome_or_x
-from ..utils import tsv_writer
+from ..generators.bed import BED, write_bed_file
 
 
 class Chromosomes(Enum):
@@ -17,6 +17,45 @@ def select_chrom_test(chroms):
         Chromosomes.AUTOSOMES_X: is_autosome_or_x,
     }
     return CHROM_TESTS.get(chroms)
+
+
+def bin_genome_chromosome(
+    genome_ref_file, output_file, chromosomes=Chromosomes.AUTOSOMES
+):
+    """This function will given a genome reference file in .2bit format,
+        create a bed file splitting the genome in bins of chromosomes.
+
+        :param genome_ref_file: File path to a .2bit file
+        :type genome_ref_file: str
+        :param output_file: The path to where the outputting bed file
+                            is stored.
+        :type output_file: str
+        :param chromosomes: Choose wich chromosomes to include in bed file.
+        :type Chromosomes:
+        """
+    tb = py2bit.open(genome_ref_file)
+    try:
+        chroms = tb.chroms()
+        beds = list()
+        for chr_name in natsorted(
+            filter(select_chrom_test(chromosomes), chroms.keys())
+        ):
+            length = int(chroms[chr_name])
+            beds.append(
+                BED(
+                    chrom=chr_name,
+                    start=0,
+                    end=length,
+                    region_id=chr_name,
+                    score=0,
+                    strand="+",
+                )
+            )
+
+        write_bed_file(output_file, beds)
+        return output_file
+    finally:
+        tb.close()
 
 
 def bin_genome_Mbp(
@@ -38,17 +77,11 @@ def bin_genome_Mbp(
     :param chromosomes: Choose wich chromosomes to include in bed file.
     :type Chromosomes:
     """
-    # NOTE: If no output file path is given, the output should prob. be
-    #       outputtet on stdout. Nice for piping.
     tb = py2bit.open(genome_ref_file)
     bin_size = int(mbp * 10 ** 6)
     try:
         chroms = tb.chroms()
-    finally:
-        tb.close()
-    with open(output_file, "w") as fp:
-        bed_writer = tsv_writer(fp)
-        bed_writer.writerow(["#chrom", "start", "end", "name", "score", "strand"])
+        beds = list()
         for chr_name in natsorted(
             filter(select_chrom_test(chromosomes), chroms.keys())
         ):
@@ -57,7 +90,16 @@ def bin_genome_Mbp(
                 range(0, length, bin_size), range(bin_size, length, bin_size)
             )
             for start, end in pos_pairs:
-                bed_writer.writerow(
-                    [chr_name, start, end, "{}_{}".format(chr_name, start), 0, "+"]
+                beds.append(
+                    BED(
+                        chrom=chr_name,
+                        start=start,
+                        end=end,
+                        region_id="{}_{}".format(chr_name, start),
+                        score=0,
+                        strand="+",
+                    )
                 )
-    return output_file
+        return output_file
+    finally:
+        tb.close()
