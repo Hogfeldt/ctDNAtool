cf42eb2c76b15ecae84b4731e7ad798330080205
Per Høgfeldt <hogfeldt38@gmail.com>
2020-11-26 07:39:30 +0100

    Added option for binning genome in chromosomes (#24)
    Co-authored-by: EliasRosendahl <jensen.eliasrosendahl@gmail.com>

--- src/ctDNAtool/generators/bed.py
+++ src/ctDNAtool/generators/bed.py
@@ -11,6 +11,18 @@ class BED:
     score = attr.ib()
     strand = attr.ib()
 
+    def __str__(self):
+        return "\t".join(
+            [
+                self.chrom,
+                str(self.start),
+                str(self.end),
+                self.region_id,
+                str(self.score),
+                self.strand,
+            ]
+        )
+
 
 def load_bed_file(file_path):
     region_lst = list()
@@ -23,3 +35,15 @@ def load_bed_file(file_path):
                 BED(line[0], int(line[1]), int(line[2]), line[3], int(line[4]), line[5])
             )
     return region_lst
+
+
+def write_bed_file(file_path, beds):
+    output_lines = ["\t".join(["#chrom", "start", "end", "name", "score", "strand"])]
+    for bed in beds:
+        output_lines.append(str(bed))
+    output_string = "\n".join(output_lines)
+    if file_path is None:
+        print(output_string)
+    else:
+        with open(file_path, "w") as fp:
+            print(output_string, file=fp)
